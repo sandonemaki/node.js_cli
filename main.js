484b3172954b6a5a857abde61043c737a705edf6
sandonemaki <kamuramaki@gmail.com>
2022-12-03 22:35:11 +0900

    不必要なコメントを削除した

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-// comanderモジュールからprogramオブジェクトをインポートする
-import {program} from "commander";
-// fs/promisesモジュールをfsオブジェクトとしてインポートする
+import { program } from "commander";
 import * as fs from "node:fs/promises";
-// markedモジュールからmarkedオブジェクトをインポートする
-import {md2html} from "./md2html.js";
+// md2htmlモジュールからmd2html関数をインポートする
+import { md2html } from "./md2html.js";
 
-// gfmオプションを定義する
 program.option("--gfm", "GFMを有効にする");
-// コマンドライン引数をcomanderでパースする
 program.parse(process.argv);
-// ファイルパスをprogam.args配列から取り出す
 const filePath = program.args[0];
 
-// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
 const cliOptions = {
-  gfm: false,
-  ...program.opts(),
+    gfm: false,
+    ...program.opts(),
 };
 
-// ファイルを非同期で読み込む
-fs.readFile(filePath, {encoding: "utf8"}).then(file => {
-  // md2htmlモジュールを使ってHTMLに変換する
-  const html = md2html(file, cliOptions);
-  console.log(html);
+fs.readFile(filePath, { encoding: "utf8" }).then(file => {
+    // md2htmlモジュールを使ってHTMLに変換する
+    const html = md2html(file, cliOptions);
+    console.log(html);
 }).catch(err => {
-  console.error(err.message);
-  // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
-  process.exit(1)
+    console.error(err.message);
+    process.exit(1);
 });
--- md2html.js
+++ md2html.js
@@ -1,7 +1,7 @@
-import {marked} from "marked";
+import { marked } from "marked";
 
 export function md2html(markdown, cliOptions) {
-  return marked.parse(markdown, {
-    gfm: cliOptions.gfm,
-  });
+    return marked.parse(markdown, {
+        gfm: cliOptions.gfm,
+    });
 };
