3c7eede03ceb83d18ac2d85d08b0c2343be06b70
sandonemaki <kamuramaki@gmail.com>
2022-12-03 22:34:17 +0900

    テストに必要な材料の余分なコードを削除した

--- test/md2html-test.js
+++ test/md2html-test.js
@@ -1,17 +1,18 @@
-import * as assert form "node:assert";
+import * as assert from "node:assert";
 import * as fs from "node:fs/promises";
-import {md2html} from "../md2html.js";
+import { md2html } from "../md2html.js";
 
 it("converts Markdown to HTML (GFM=false)", async() => {
-  // fs.readFileはPromiseを返すので、`await`式で読み込みが完了するまで待って内容を取得する
-  const sample = await fs.readFile("test/fixtures/sample.md", {encoding: "utf8"});
-  const expected = await fs.readFile("test/fixtures/expected.html", {encoding: "utf8"}
-    // 末尾の改行の有無の違いを無視するため、変換後のHTMLのスペースをtrimメソッドで解除してから比較しています。
-    assert.strictEqual(md2html(sample, {gfm: false})).trimEnd(), expected.trimEnd());
+    // fs.readFileはPromiseを返すので、`await`式で読み込みが完了するまで待って内容を取得する
+    const sample = await fs.readFile("test/fixtures/sample.md", { encoding: "utf8" });
+    const expected = await fs.readFile("test/fixtures/expected.html", { encoding: "utf8" });
+    // 末尾の改行の有無の違いを無視するため、変換後のHTMLのスペースをtrimメソッドで削除してから比較しています
+    assert.strictEqual(md2html(sample, { gfm: false }).trimEnd(), expected.trimEnd());
 });
 
 it("converts Markdown to HTML (GFM=true)", async() => {
-  const sample = await fs.readFile("test/fixtures/sample.md", {encoding: "utf8"});
-  const expected = await fs.readFile("test/fixtures/expected-gfm.html", {encoding: "utf8"}
-    assert.strictEqual(md2html(sample, {gtm: true}).trimEnd(), expected.trimEnd());
+    const sample = await fs.readFile("test/fixtures/sample.md", { encoding: "utf8" });
+    const expected = await fs.readFile("test/fixtures/expected-gfm.html", { encoding: "utf8" });
+    // 末尾の改行の有無の違いを無視するため、変換後のHTMLのスペースをtrimメソッドで削除してから比較しています
+    assert.strictEqual(md2html(sample, { gfm: true }).trimEnd(), expected.trimEnd());
 });
