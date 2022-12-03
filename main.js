10fc7f148358678f192813f97f4f695f1ba2c765
sandonemaki <kamuramaki@gmail.com>
2022-12-03 16:38:50 +0900

    fs/promiseモジュールをインポートしてファイルを読み込んだ

---
+++ main.js
@@ -0,0 +1,16 @@
+// comanderモジュールからprogramオブジェクトをインポートする
+import {program} from "commander";
+// fs/promisesモジュールをfsオブジェクトとしてインポートする
+import * as fs from "node:fs/promises";
+
+
+// コマンドライン引数をcomanderでパースする
+program.parse(process.argv);
+
+// ファイルパスをwprogam.args配列から取り出す
+const filePath = program.args[0];
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath).then(file => {
+  console.log(file);
+});
