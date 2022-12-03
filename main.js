3a7961e3105207009ec459e6d269a0cbee4b4e11
sandonemaki <kamuramaki@gmail.com>
2022-12-03 17:38:07 +0900

    gfmオプションを定義、取得し、MarkdownファイルをHTMLに変換して、その結果を標準出力に表示した

--- main.js
+++ main.js
@@ -5,16 +5,29 @@ import * as fs from "node:fs/promises";
 // markedモジュールからmarkedオブジェクトをインポートする
 import {marked} from "marked";
 
-
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 // コマンドライン引数をcomanderでパースする
 program.parse(process.argv);
 
-// ファイルパスをwprogam.args配列から取り出す
+// ファイルパスをprogam.args配列から取り出す
 const filePath = program.args[0];
+
+// コマンドライン引数のオプションを取得する
+const options = program.opts();
+
+// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
+const cliOptions = {
+  gfm: options.gfm ?? false,
+};
 
 // ファイルを非同期で読み込む
 fs.readFile(filePath, {encoding: "utf8"}).then(file => {
-  console.log(file);
+  const html = marked.parse(file, {
+    // オプションの値を使用する
+    gfm: cliOptions.gfm,
+  });
+  console.log(html);
 }).catch(err => {
   console.error(err.message);
   // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
